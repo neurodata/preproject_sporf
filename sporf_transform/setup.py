976c4580c8433fdf0dd0b549e359748d50341860
Ben Falk <falk.ben@gmail.com>
2019-11-08 14:55:36 -0500

    a little more info in readme
    added gitignore

--- sporf_transform/setup.py
+++ sporf_transform/setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
+from distutils.core import Extension, setup
 
+import numpy
+from Cython.Build import cythonize
 
 extensions = [
-        Extension("sporf_transform", sources=["_project_sporf.pyx"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),
-        ]
+    Extension(
+        "sporf_transform",
+        sources=["_project_sporf.pyx"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=["-O3"],
+    )
+]
 
 
-setup(
-    name="_project_sporf",
-    ext_modules = cythonize(extensions),
-)
-
-
+setup(name="project_sporf", ext_modules=cythonize(extensions))
